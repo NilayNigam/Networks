25b5239c3fb676623ef12977398580d9a3c0e41a
NilayNigam <nilay.nigam@gmail.com>
2020-12-03 14:31:17 -0500

    Add files via upload

---
+++ run_app.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#python sklearn_classifiers/run_training.py \ 
+#    --train_dataset csv_files/example_20packets.csv \  
+#    --target_column ndpi_category \
+#    --continuous#!/bin/bash
+
+MODEL_TIME_ID=$1
+
+#export CUDA_VISIBLE_DEVICES=4
+
+export DATA_DIR="$PWD/csv_files/example_20packets.csv"
+#export SAVED_DIR="$PWD/results/sentihood/NLI_M"
+#export PRED_DATA_DIR="$PWD/results/sentihood/NLI_M/test_ep_4.txt"
+
+#export VOCAB_DATA = "$PWD/uncased_L-12_H-768_A-12/vocab.txt"
+#export CFG_FILE = "$PWD/uncased_L-12_H-768_A-12/bert_config.json"
+#export CKP$PWD/csv_files/example_20packets.csvT_FILE = "$PWD/uncased_L-12_H-768_A-12/pytorch_model.bin"
+
+
+
+#cd src
+
+echo "Applying model..."
+python sklearn_classifiers/run_training.py \
+--train_dataset $DATA_DIR \
+--target_column ndpi_category \
+--continuous
+
+echo "All done! :)"
+
